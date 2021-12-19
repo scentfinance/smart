59fc8148755473aa577e0f504a3abd64f56c4a8e
flashdebugger <flashdebugger0330@gmail.com>
2021-12-19 00:57:52 -0500

    Update battle contract with basic deploy and test scripts

--- battle/contracts/Battle.sol
+++ battle/contracts/Battle.sol
@@ -1,7 +1,7 @@
-//SPDX-License-Identifier: Unlicense
+//SPDX-License-Identifier: GNU Public License
 pragma solidity ^0.8.0;
 
-import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "hardhat/console.sol";
 
 contract Battle {
@@ -20,13 +20,14 @@ contract Battle {
         string[] cities;
     }
     mapping(address => Player) private players;
+    uint256 public players_count;
 
     constructor(string[] memory _countries, address _token) {
-        console.log("Deploying a Battle with countries: ", _countries);
         countries = _countries;
 
         console.log("Deploying a Battle with main token: ", _token);
         token = _token;
+        players_count = 0;
     }
 
     function deposit(uint256 _amount) external {
@@ -34,25 +35,25 @@ contract Battle {
             IERC20(token).transferFrom(msg.sender, address(this), _amount),
             "Transfer from failed"
         );
-        balances[msg.sender] = balances[msg.sender].add(_amount);
+        balances[msg.sender] = balances[msg.sender] + _amount;
         emit Deposit(msg.sender, _amount);
     }
 
     function withdraw(uint256 _amount) external {
         require(balances[msg.sender] >= _amount, "Withdraw amount failed");
         require(IERC20(token).transfer(msg.sender, _amount), "Transfer failed");
-        balances[msg.sender] = balances[msg.sender].sub(_amount);
+        balances[msg.sender] = balances[msg.sender] - _amount;
         emit Withdraw(msg.sender, _amount);
     }
 
     function registerPlayer(
-        uint32 _soliders,
+        uint32 _soldiers,
         uint32 _tanks,
         uint32 _generals,
-        string[] _countries,
-        string[] _cities
+        string[] memory _countries,
+        string[] memory _cities
     ) external {
-        require(players.length() <= 5, "All players joined");
+        require(players_count <= 5, "All players joined");
         players[msg.sender] = Player(
             msg.sender,
             _soldiers,
@@ -61,24 +62,28 @@ contract Battle {
             _countries,
             _cities
         );
+        players_count += 1;
     }
 
-    function reset(string[] memory _countries, address _token) {
-        console.log("Deploying a Battle with countries: ", _countries);
+    function reset(string[] memory _countries, address _token) public {
         countries = _countries;
 
         console.log("Deploying a Battle with main token: ", _token);
         token = _token;
-
-        players = [];
     }
 
-    function greet() public view returns (string memory) {
-        return greeting;
+    function getCountries() public view returns (string[] memory) {
+        return countries;
     }
 
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+    function getTokenAddress() public view returns (address) {
+        return token;
     }
+
+    // function setGreeting(string memory _greeting) public {
+    //     console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
+    //     greeting = _greeting;
+    // }
+    event Deposit(address holder, uint256 amount);
+    event Withdraw(address holder, uint256 amount);
 }
